9899ea773cdc9d73638212146978419bda45372a
KritikaBhunwal <kritikabhunwal@gmail.com>
2025-05-10 23:16:31 +0530

    Compressed Images

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,16 +2,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import ServiceIntroGrid from '../components/ServiceIntroGrid';
-import ValuesRow from '../components/ValuesRow';
+import ImageBanner from '../components/ImageBanner';
+import gallery7 from '../assets/gallery7.jpeg';
 
 export default function Services() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
+              <ImageBanner src={gallery7} alt="Banner backdrop" />
       <div style={{ flex: 1 }}>
-                <ServiceIntroGrid />
-                <ValuesRow />
       </div>
       <Footer />
     </div>
